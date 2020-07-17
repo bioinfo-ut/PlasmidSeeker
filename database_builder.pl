c848ec5154b661bce14dee4d90d4fd5ee9da1fac
mihkelvaher <mihkel.vaher@ut.ee>
2020-07-17 08:45:55 +0300

    while building the db, add plasmid length to names.txt

--- database_builder.pl
+++ database_builder.pl
@@ -6,11 +6,13 @@
 use warnings;
 use strict;
 use Getopt::Long;
+use FindBin;
+use File::Basename;
 
 # GLOBALS
 
 my $fastafile;
-my $glistmaker = "GenomeTester4/glistmaker";
+my $glistmaker = "$FindBin::RealBin/GenomeTester4/glistmaker";
 my $dir_location = "plasmid_db";
 my $help;
 my $version;
@@ -31,11 +33,12 @@ GetOptions(
 	't=s' => \$threads,
      ) or die printHelp()."\n";
 
-if ($version){ die "PlasmidSeeker builder v1.0 (16 May 2017)\n"; }
+if ($version){ die "PlasmidSeeker builder v1.1 (July 2020)\n"; }
 
 # Check presence of options and files
-if(!$dir_location || !$fastafile) { die printHelp()."\n"; }
+die printHelp()."\n" if(!$dir_location || !$fastafile);
 
+die "Can't find glistmaker at $glistmaker" if !-e $glistmaker;
 ##########
 ### SUBS
 ##########
@@ -60,7 +63,7 @@ sub printHelp {
 ###### Step 1 - extracts all plasmids as single fna files to a tmp directory
 print STDERR "Getting all individual plasmid sequences and putting them to $dir_location\_fna\n";
 open(FNA,'<',$fastafile) or die("Please specify plasmid fasta file!");
-system "mkdir $dir_location\_fna";
+system "mkdir $dir_location\_fna" if !-d "$dir_location\_fna";
 
 my $count = 1;
 while(<FNA>) {
@@ -74,17 +77,15 @@ while(<FNA>) {
 
 ###### Step 2 - converts each fna file to a list file (multithreaded)
 print STDERR "Converting plasmid fna sequences to list files in $dir_location\n";
-opendir my $dir, "$dir_location\_fna" or die "Cannot open temporary fna dir $!";
-my @fna_files = grep { $_ ne '.' && $_ ne '..' } readdir $dir;
-closedir $dir;
-system "mkdir $dir_location";
+my @fna_files = glob("$dir_location\_fna/*");
+system "mkdir $dir_location" if !-d $dir_location;
 
 $count = 0;
 my $cmd;
 my $total = 0;
 foreach (@fna_files) {
 	$count++;
-	$cmd .= "$glistmaker $dir_location\_fna\/$_ -w $word -o $dir_location\/$_ & ";
+	$cmd .= "$glistmaker $_ -w $word -o $dir_location/".basename($_)." & ";
 	#Multithread execution if thread limit reached
 	if ($count == $threads) {
 		$total = $total + $threads;
@@ -107,14 +108,16 @@ $count = scalar(@fna_files);
 print FILE "# Database: $dir_location\tPlasmids total: $count\tBuilt on: $time\tK-mer length: $word\n#\n"; # Print all info to header
 foreach(@fna_files) {
 	chomp;
-	my $cmd = "head -n 1 $dir_location\_fna\/$_";
-	my $head = qx/$cmd/;
+	my $cmd = "head -n 1 $_";
+	my $head = `$cmd`;
+	my $length = `awk '/^>/{if (l!="") print l; l=0; next}{l+=length(\$0)}END{print l}' $_`;
 	#$head = (split(/\|\s+/,$head))[1]; Split only in RefSeq format
 	chomp $head;
-	print FILE "$_\t$head\n";
+	chomp $length;
+	print FILE basename($_)."\t$head\t$length\n";
 }
 close FILE;
 
 ## FINAL - remove fna files and dir, leave only db lists
-system "rm $dir_location\_fna/*.fna";
-system "rmdir $dir_location\_fna";
+#system "rm $dir_location\_fna/*.fna";
+#system "rmdir $dir_location\_fna";
